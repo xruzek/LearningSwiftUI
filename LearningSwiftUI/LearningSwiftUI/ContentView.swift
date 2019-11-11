538f4b041e8a201eb58bb07b6bea957c6e95bada
xruzek <rexruzek@gmail.com>
2019-11-11 09:08:22 -0500

    Add files via upload

---
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningSwiftUI
+//
+//  Created by Rex Ruzek on 11/11/19.
+//  Copyright © 2019 Rex Ruzek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
